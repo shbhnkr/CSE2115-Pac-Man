25dc96e57ed3ae051384a9633095eb7f8f2b75f0
Swastik <s.agarwal-2@student.tudelft.nl>
2019-11-21 16:14:20 +0100

    made the register screen better

--- src/main/java/Register.java
+++ src/main/java/Register.java
@@ -1,8 +1,11 @@
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import javax.swing.*;
-
 
 
 public class Register {
@@ -12,13 +15,34 @@ public class Register {
     private transient JTextField enterUsernameTextField;
     private transient JPasswordField enterPasswordPasswordField;
     private transient JTextArea pacmanTextArea;
+    private transient Font font;
 
     /**
      * Constructor.
      */
     public Register() {
-        Dimension dimension = new Dimension(20, 20);
+
+        URL path = ClassLoader.getSystemResource("crackman.ttf");
+        File file = new File(path.getFile());
+        try {
+            font = Font.createFont(Font.TRUETYPE_FONT, file);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
+
+        Dimension dimension = new Dimension(5, 5);
         enterUsernameTextField.setPreferredSize(dimension);
+        enterPasswordPasswordField.setPreferredSize(dimension);
+        Color color = new Color(255, 255, 255);
+        enterUsernameTextField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, color));
+        enterPasswordPasswordField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, color));
+        button1.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, color));
+        pacmanTextArea.setFont(new Font("crackman", Font.PLAIN, 35));
+        button1.setFont(new Font("crackman", Font.PLAIN, 20));
+        button1.setCursor(Cursor.getPredefinedCursor(12));
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
