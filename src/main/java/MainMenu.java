70fa08ec4256ca459ed461c9549727c4353a58d6
Shubhankar <s.darbari@student.tudelft.nl>
2020-01-14 14:11:07 +0100

    PMD fix

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 import java.net.URL;
 
 public class MainMenu {
-    private JPanel panel1;
-    private JButton playGameButton;
-    private JButton logoutButton;
-    private JComboBox comboBox1;
-    private JTextArea title;
-    private JPanel playerDetails;
-    private JTextArea textArea1;
-    private JTextArea textArea2;
-    private Font font;
+    private transient JPanel panel1;
+    private transient JButton playGameButton;
+    private transient JButton logoutButton;
+    private transient JComboBox comboBox1;
+    private transient JTextArea title;
+    private transient JPanel playerDetails;
+    private transient JTextArea textArea1;
+    private transient JTextArea textArea2;
+    private transient Font font;
     
     public MainMenu() {
         URL path = ClassLoader.getSystemResource("crackman.ttf");
@@ -26,14 +26,14 @@ public class MainMenu {
             e.printStackTrace();
         }
         GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
-
-        playGameButton.setFont(new Font("crackman", Font.PLAIN, 20));
-        logoutButton.setFont(new Font("crackman", Font.PLAIN, 20));
-        title.setFont(new Font("crackman", Font.PLAIN, 35));
+        String fontName = "crackman";
+        playGameButton.setFont(new Font(fontName, Font.PLAIN, 20));
+        logoutButton.setFont(new Font(fontName, Font.PLAIN, 20));
+        title.setFont(new Font(fontName, Font.PLAIN, 35));
 
         Color color = new Color(0, 0, 0);
 
-        comboBox1.setFont(new Font("crackman", Font.PLAIN, 15));
+        comboBox1.setFont(new Font(fontName, Font.PLAIN, 15));
 
         comboBox1.setBorder(BorderFactory.createMatteBorder(0,0,0,0,color));
         logoutButton.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, color));
