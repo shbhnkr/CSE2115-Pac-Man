756c75f95c4cf8068910aa523d2f7de960bda5c4
Lesley Verdoner <L.franschman@student.tudelft.nl>
2020-01-19 01:20:57 +0100

    fix

--- src/test/java/game/GameCollisionTest.java
+++ src/test/java/game/GameCollisionTest.java
@@ -10,7 +10,7 @@ class GameCollisionTest {
 
     @Test
     public void win() {
-        Game game = new Game(new GameSettings(20), "winBoard.txt");
+        Game game = new Game(new GameSettings(20, GameSettings.getUsername()), "winBoard.txt");
         game.moveRight();
         System.out.println(game);
         Assertions.assertEquals(game.pelletEaten, game.pelletCount);
@@ -20,7 +20,7 @@ class GameCollisionTest {
 
     @Test
     public void pelletCollision() {
-        Game game = new Game(new GameSettings(20), "board2.txt");
+        Game game = new Game(new GameSettings(20, GameSettings.getUsername()), "board2.txt");
         Assertions.assertEquals(0, game.pelletEaten);
         game.moveLeft();
         game.moveRight();
@@ -41,7 +41,7 @@ class GameCollisionTest {
 
     @Test
     public void wallCollision() {
-        Game game = new Game(new GameSettings(20), "wallBoard.txt");
+        Game game = new Game(new GameSettings(20, GameSettings.getUsername()), "wallBoard.txt");
         Point loc = game.player.getLocation();
         game.moveLeft();
         Assertions.assertEquals(game.player.getLocation(), loc);
@@ -56,62 +56,62 @@ class GameCollisionTest {
 
     @Test
     public void wrapAroundTest() {
-        Game game = new Game(new GameSettings(20), "wrapLp.txt");
+        Game game = new Game(new GameSettings(20, GameSettings.getUsername()), "wrapLp.txt");
         Assertions.assertEquals(0, game.pelletEaten);
         game.moveLeft();
         game.moveLeft();
         Assertions.assertEquals(1, game.pelletEaten);
         game.stop();
-        Game game1 = new Game(new GameSettings(20), "wrapUp.txt");
+        Game game1 = new Game(new GameSettings(20, GameSettings.getUsername()), "wrapUp.txt");
         Assertions.assertEquals(0, game1.pelletEaten);
         game1.moveUp();
         game1.moveUp();
         Assertions.assertEquals(1, game1.pelletEaten);
         game1.stop();
-        Game game2 = new Game(new GameSettings(20), "wrapDp.txt");
+        Game game2 = new Game(new GameSettings(20, GameSettings.getUsername()), "wrapDp.txt");
         Assertions.assertEquals(0, game2.pelletEaten);
         game2.moveDown();
         game2.moveDown();
         Assertions.assertEquals(1, game2.pelletEaten);
         game2.stop();
-        Game game3 = new Game(new GameSettings(20), "wrapRp.txt");
+        Game game3 = new Game(new GameSettings(20, GameSettings.getUsername()), "wrapRp.txt");
         Assertions.assertEquals(0, game3.pelletEaten);
         game3.moveRight();
         game3.moveRight();
         Assertions.assertEquals(1, game3.pelletEaten);
         game3.stop();
-        Game game4 = new Game(new GameSettings(20), "wrapLw.txt");
+        Game game4 = new Game(new GameSettings(20, GameSettings.getUsername()), "wrapLw.txt");
         game4.moveLeft();
         Point loc = game4.player.getLocation();
         game4.moveLeft();
         Assertions.assertEquals(game4.player.getLocation(), loc);
         game4.stop();
-        Game game5 = new Game(new GameSettings(20), "wrapRw.txt");
+        Game game5 = new Game(new GameSettings(20, GameSettings.getUsername()), "wrapRw.txt");
         game5.moveRight();
         Point loc1 = game5.player.getLocation();
         game5.moveRight();
         Assertions.assertEquals(game5.player.getLocation(), loc1);
         game5.stop();
-        Game game6 = new Game(new GameSettings(20), "wrapDw.txt");
+        Game game6 = new Game(new GameSettings(20, GameSettings.getUsername()), "wrapDw.txt");
         game6.moveDown();
         Point loc2 = game6.player.getLocation();
         game6.moveDown();
         Assertions.assertEquals(game6.player.getLocation(), loc2);
         game6.stop();
-        Game game7 = new Game(new GameSettings(20), "wrapUw.txt");
+        Game game7 = new Game(new GameSettings(20, GameSettings.getUsername()), "wrapUw.txt");
         Assertions.assertEquals(game7.player.getLocation(), new Point(20, 20));
         game7.moveUp();
         Point loc3 = game7.player.getLocation();
         game7.moveUp();
         Assertions.assertEquals(game7.player.getLocation(), loc3);
         game7.stop();
-        Game game8 = new Game(new GameSettings(20), "board3.txt");
+        Game game8 = new Game(new GameSettings(20, GameSettings.getUsername()), "board3.txt");
         game8.stop();
     }
 
     @Test
     public void fruitPelletCollision() {
-        Game game = new Game(new GameSettings(20), "TestWall.txt");
+        Game game = new Game(new GameSettings(20, GameSettings.getUsername()), "TestWall.txt");
         Assertions.assertEquals(2, game.pelletCount);
         Assertions.assertEquals(0, game.pelletEaten);
         game.moveRight();
