e8ac4838653a1d4a32466cf3efe9da27b5e19382
Lesley Verdoner <L.franschman@student.tudelft.nl>
2020-01-20 12:43:31 +0100

    fix equal score problem

--- src/main/java/database/DBconnection.java
+++ src/main/java/database/DBconnection.java
@@ -57,6 +57,9 @@ public class DBconnection {
             try {
                 rsmd = rs.getMetaData();
                 int columnsNumber = rsmd.getColumnCount();
+                if(columnsNumber > 3){
+                    columnsNumber /= 2;
+                }
                 while (rs.next()) {
                     for (int i = 1; i <= columnsNumber; i++) {
                         if (i > 1) System.out.print(",  ");
