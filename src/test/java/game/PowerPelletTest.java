7681a081f84ecd30d1e06e48e3de10fedb07dd21
Swastik <s.agarwal-2@student.tudelft.nl>
2020-01-22 23:54:46 +0100

    tests

---
+++ src/test/java/game/PowerPelletTest.java
@@ -0,0 +1,132 @@
+package game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+public class PowerPelletTest {
+    @Test
+    public void gallbossCollisionLeft() {
+
+        Game game = new Game(new Gamesettings(20, null), "poweredPelletTest.txt");
+        Player player = game.player;
+        player.moveLeft(game, game.getWidth());
+        Assertions.assertTrue(player.power);
+        Point loc = player.getLocation();
+        player.moveLeft(game, game.getWidth());
+        Assertions.assertNotEquals(player.getLocation(),loc);
+    }
+    @Test
+    public void gallbossCollisionRight() {
+
+        Game game = new Game(new Gamesettings(20, null), "poweredPelletTest.txt");
+        Player player = game.player;
+        player.moveRight(game, game.getWidth());
+        Assertions.assertTrue(player.power);
+        Point loc = player.getLocation();
+        player.moveRight(game, game.getWidth());
+        Assertions.assertNotEquals(player.getLocation(),loc);
+    }
+    @Test
+    public void gallbossCollisionUp() {
+
+        Game game = new Game(new Gamesettings(20, null), "poweredPelletTest.txt");
+        Player player = game.player;
+        player.moveUp(game, game.getHeight());
+        Assertions.assertTrue(player.power);
+        Point loc = player.getLocation();
+        player.moveUp(game, game.getHeight());
+        Assertions.assertNotEquals(player.getLocation(),loc);
+    }
+    @Test
+    public void gallbossCollisionDown() {
+
+        Game game = new Game(new Gamesettings(20, null), "poweredPelletTest.txt");
+        Player player = game.player;
+        player.moveDown(game, game.getHeight());
+        Assertions.assertTrue(player.power);
+        Point loc = player.getLocation();
+        player.moveDown(game, game.getHeight());
+        Assertions.assertNotEquals(player.getLocation(),loc);
+    }
+
+    @Test
+    public void beerCollisionLeft() {
+
+        Game game = new Game(new Gamesettings(20, null), "beerTest.txt");
+        Player player = game.player;
+        player.moveLeft(game, game.getWidth());
+        Assertions.assertTrue(player.drunk);
+
+    }
+    @Test
+    public void beerCollisionRight() {
+
+        Game game = new Game(new Gamesettings(20, null), "beerTest.txt");
+        Player player = game.player;
+        player.moveRight(game, game.getWidth());
+        Assertions.assertTrue(player.drunk);
+
+    }
+    @Test
+    public void beerCollisionUp() {
+
+        Game game = new Game(new Gamesettings(20, null), "beerTest.txt");
+        Player player = game.player;
+        player.moveUp(game, game.getHeight());
+        Assertions.assertTrue(player.drunk);
+
+    }
+    @Test
+    public void beerCollisionDown() {
+
+        Game game = new Game(new Gamesettings(20, null), "beerTest.txt");
+        Player player = game.player;
+        player.moveDown(game, game.getHeight());
+        Assertions.assertTrue(player.drunk);
+
+    }
+
+    @Test
+    public void fruitCollisionLeft() {
+
+        Game game = new Game(new Gamesettings(20, null), "fruitTest.txt");
+        Player player = game.player;
+        int n = game.point;
+        player.moveLeft(game, game.getWidth());
+        Assertions.assertNotEquals(n,game.point);
+
+    }
+    @Test
+    public void fruitCollisionRight() {
+
+        Game game = new Game(new Gamesettings(20, null), "fruitTest.txt");
+        Player player = game.player;
+        int n = game.point;
+        player.moveRight(game, game.getWidth());
+        Assertions.assertNotEquals(n,game.point);
+    }
+    @Test
+    public void fruitCollisionUp() {
+
+        Game game = new Game(new Gamesettings(20, null), "fruitTest.txt");
+        Player player = game.player;
+        int n = game.point;
+        player.moveUp(game, game.getHeight());
+        Assertions.assertNotEquals(n,game.point);
+
+    }
+    @Test
+    public void fruitCollisionDown() {
+
+        Game game = new Game(new Gamesettings(20, null), "fruitTest.txt");
+        Player player = game.player;
+        int n = game.point;
+        player.moveDown(game, game.getHeight());
+        Assertions.assertNotEquals(n,game.point);
+
+    }
+
+
+}
